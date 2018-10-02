9769d9c355ccdcf4898d2fbc0eb50c6180826ad1
unknown <victorst79@gmail.com>
2018-10-02 10:57:18 +0200

    Improvements Launcher 3

--- initial.py
+++ initial.py
@@ -13,3 +13,10 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Wito-Bot on Duty')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')

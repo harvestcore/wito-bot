4e83e3d2a80d14ac332f346d22f4f93b9e8e7552
unknown <victorst79@gmail.com>
2018-10-02 10:48:30 +0200

    Improvements Launcher 2

--- initial.py
+++ initial.py
@@ -6,4 +6,10 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-#the bot doesnt talk to itself
+    #the bot doesnt talk to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
